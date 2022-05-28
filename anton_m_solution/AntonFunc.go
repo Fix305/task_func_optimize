eaf25b81fc419e155bb960e0f9361c3f68c8e7b3
Anton M <fix305@gmail.com>
2022-05-28 14:55:57 +0500

    add fast variant

--- anton_m_solution/AntonFunc.go
+++ anton_m_solution/AntonFunc.go
@@ -1,5 +1,7 @@
 package anton_m_solution
 
+import "math"
+
 // MySuperFuncImpl MySuperFunc - реализация
 // 1. `n==0` -> `x1`
 // 2. `n==1` -> `x1 * x2`
@@ -10,7 +12,31 @@ var cache = make(map[uint8]float64)
 func MySuperFuncImpl(x1 float64, x2 float64, n uint8) float64 {
 	cache = make(map[uint8]float64)
 
+	// Более шустрый вариант, но 2 теста не проходит по точности вычисления
+	/// return flat(x1, x2, n)
 	return recursive(x1, x2, n)
+}
+
+// Более шустрый вариант, но 2 теста не проходит по точности вычисления
+func flat(x1 float64, x2 float64, n uint8) float64 {
+	var lastX1coef uint8 = 1
+	var lastX2coef uint8 = 0
+	var currentX1coef uint8 = 0
+	var currentX2coef uint8 = 0
+
+	for i := uint8(1); i <= n; i++ {
+		currentX1coef = lastX1coef + lastX2coef
+		currentX2coef = lastX1coef
+
+		lastX1coef = currentX1coef
+		lastX2coef = currentX2coef
+	}
+
+	if n == 0 {
+		return math.Pow(x1, float64(lastX1coef))
+	} else {
+		return math.Pow(x1, float64(lastX1coef)) * math.Pow(x2, float64(lastX2coef))
+	}
 }
 
 func recursive(x1 float64, x2 float64, n uint8) float64 {
